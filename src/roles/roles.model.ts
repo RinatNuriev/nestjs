5fb23691a08d9ab0d1bf00faee181176a6b50f46
RinatNuriev <justinnuriev@gmail.com>
2024-02-09 11:10:23 +0300

    commit

---
+++ src/roles/roles.model.ts
@@ -0,0 +1,48 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import { ApiProperty } from '@nestjs/swagger';
+
+interface UserCreationAttrs {
+  email: string;
+  password: string;
+}
+
+@Table({ tableName: 'users' })
+export class User extends Model<User, UserCreationAttrs> {
+  @ApiProperty({ example: '1', description: 'Уникальный индентификатор' })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @ApiProperty({ example: 'test@mail.ru' })
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  email: string;
+
+  @ApiProperty({ example: 'password' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  password: string;
+
+  @ApiProperty({ example: 'true' })
+  @Column({
+    type: DataType.BOOLEAN,
+    defaultValue: false,
+  })
+  banned: boolean;
+
+  @ApiProperty({ example: 'За что' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  banReason: string;
+}
